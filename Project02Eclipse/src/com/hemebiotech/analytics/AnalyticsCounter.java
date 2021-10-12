44a90bcef1b6a6d849fa6fd3e927c775d1ab7ad0
Chanwoo KIM <chanwoo.kim@outlook.fr>
2021-10-12 19:03:27 +0200

    Fix: Fix AnalyticsCounter.java

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,32 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.File;
+import java.util.List;
 
+/**
+ * This class should create a result file.
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	/**
+	 *
+	 * @param args is a string array of files' paths. This is not mandatory but if you use this parameter,
+	 * args[0] should be an input file path and args[1] should be an output file path.
+	 *
+	 */
+	public static void main(String args[]) {
+		// If there is no main's parameter, use these paths for input and output path
+		String symptomsPath = "Project02Eclipse" + File.separator + "symptoms.txt";
+		String resultPath = "result.out";
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		boolean argsExist = args.length != 0; // Verify existence of main's parameter
+		ReadSymptomDataFromFile readSymptom = argsExist ?
+				new ReadSymptomDataFromFile(args[0]) :
+				new ReadSymptomDataFromFile(symptomsPath);
+		List<String> symptomsList = readSymptom.getSymptoms(); // Get a list of symptoms from the instance
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		WriteSymptomDataToFile writeSymptom = argsExist ?
+				new WriteSymptomDataToFile(symptomsList, args[1]) :
+				new WriteSymptomDataToFile(symptomsList, resultPath);
+		writeSymptom.createFile(); // Create a file from the instance
 	}
 }
