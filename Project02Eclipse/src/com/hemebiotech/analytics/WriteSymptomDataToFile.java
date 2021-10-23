ee05526a7210a43a7dd429e75aaa59305c785bfb
Chanwoo KIM <chanwoo.kim@outlook.fr>
2021-10-23 18:01:26 +0200

    Refactor: Refactor method createFile

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,17 +1,18 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
 
 /**
  * Implement ISymptomWriter
  *
  */
 public class WriteSymptomDataToFile implements ISymptomWriter {
-    private final List<String> SYMPTOMS;
-    private final String FILENAME;
+    private final List<String> symptoms;
+    private final String filename;
 
     /**
      *
@@ -19,21 +20,19 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
      * @param filename is a filename that will be created.
      */
     WriteSymptomDataToFile(List<String> symptoms, String filename) {
-        this.SYMPTOMS = new ArrayList<>(symptoms);
-        this.FILENAME = filename;
+        this.symptoms = new ArrayList<>(symptoms);
+        this.filename = filename;
     }
 
     @Override
     public void createFile() {
         TreeMap<String, Integer> map = sortSymptom(); // Get TreeMap from sortSymptom
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME))) {
-            Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
-            while(it.hasNext()) {
-                Map.Entry<String, Integer> entry = it.next();
-                writer.write(String.format("%s=%d", entry.getKey(), entry.getValue()));
-                writer.newLine();
-            }
-        } catch(IOException e) {
+
+        try(PrintWriter pw = new PrintWriter(filename, "utf-8")) {
+            map.forEach((k,v) -> { // k -> key(Symptom), v -> value(count occurrences)
+                pw.println(String.format("%s=%d",k,v));
+            });
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -46,7 +45,7 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
     private TreeMap<String, Integer> sortSymptom() {
         TreeMap<String, Integer> map = new TreeMap<>(); // String is symptom, Integer is count occurrences of a symptom
 
-        for (String symptom : SYMPTOMS) {
+        for (String symptom : symptoms) {
             if (map.containsKey(symptom)) {
                 Integer cnt = map.get(symptom); // Get symptom's value(count occurrences)
                 map.put(symptom, cnt + 1); // old value + 1
